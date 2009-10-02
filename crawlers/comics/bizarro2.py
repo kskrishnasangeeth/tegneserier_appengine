d13143ebb36fab6589f8ef37da13bc24387b19df
Georg Alexander Boe <georg.boe@gmail.com>
2009-10-02 18:26:28 +0200

    Changed timezone for Bizarro2.

--- crawlers/comics/bizarro2.py
+++ crawlers/comics/bizarro2.py
@@ -3,5 +3,6 @@ from crawlers.base import BaseComicCrawler
 class Bizarro2(BaseComicCrawler):
 	def __init__(self):
 		super(Bizarro2, self).__init__('ag9taW5ldGVnbmVzZXJpZXJyCwsSBUNvbWljGAIM')
+		self.timezone = 2
 		self.url = "http://cserver.it-content.com/retriever.php?id=87&date=%s" % (self.date.strftime("%Y%m%d"))
 		
