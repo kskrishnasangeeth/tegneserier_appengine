20a67016f455272ddec5c2333d20e3450a69ef0a
Georg Alexander Boe <georg.boe@gmail.com>
2009-10-02 18:18:31 +0200

    Corrected the Bizarro3 key.

--- crawlers/comics/bizarro3.py
+++ crawlers/comics/bizarro3.py
@@ -2,6 +2,6 @@ from crawlers.base import BaseComicCrawler
 
 class Bizarro3(BaseComicCrawler):
 	def __init__(self):
-		super(Bizarro3, self).__init__('ag9taW5ldGVnbmVzZXJpZXJyCwsSBUNvbWljGAIM')
+		super(Bizarro3, self).__init__('ag9taW5ldGVnbmVzZXJpZXJyCwsSBUNvbWljGAkM')
 		self.headers = { "Referer": 'http://www.kingfeatures.com/features/comics/bizarro/about.htm' }
 		self.url = "http://est.rbma.com/content/Bizarro?date=%s" % (self.date.strftime("%Y%m%d"))
