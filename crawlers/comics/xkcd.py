73a67cf8487eff0d8b8b0973ac1ddad580cf7acf
Georg Alexander Boe <georg.boe@gmail.com>
2009-10-03 12:48:28 +0200

    Added xkcd.

---
+++ crawlers/comics/xkcd.py
@@ -0,0 +1,16 @@
+from crawlers.base import BaseComicCrawler
+from google.appengine.api import urlfetch
+from BeautifulSoup import BeautifulSoup
+
+class Xkcd(BaseComicCrawler):
+	def __init__(self):
+		super(Xkcd, self).__init__('ag9taW5ldGVnbmVzZXJpZXJyCwsSBUNvbWljGA0M')
+		self.url = self._imagelink()
+	
+	def _imagelink(self):
+		content = urlfetch.fetch('http://www.xkcd.com').content
+		soup = BeautifulSoup(content)
+		for img in soup.findAll('img'):
+			if img["src"].startswith("http://imgs.xkcd.com/comics/"):
+				return img["src"]
+		raise Exception
