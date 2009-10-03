c0b08e0e04eca6a669ccf0e406822a539a3f02a0
Georg Alexander Boe <georg.boe@gmail.com>
2009-10-03 14:20:39 +0200

    Comic.py is now longer need since all the required information is in picture.py

--- models/comic.py
+++
@@ -1,6 +0,0 @@
-from google.appengine.ext import db
-
-class Comic(db.Model):
-	name = db.StringProperty(required=True)
-	group = db.StringProperty(required=True)
-	sort_order = db.IntegerProperty(required=True)
