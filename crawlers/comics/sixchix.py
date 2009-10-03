c3e7703d03a8de09ae6ebd1720c186e3404e47c4
Georg Alexander Boe <georg.boe@gmail.com>
2009-10-03 15:04:02 +0200

    Fixed typo in model name.

--- crawlers/comics/sixchix.py
+++ crawlers/comics/sixchix.py
@@ -2,5 +2,5 @@ from crawlers.base import BaseComicCrawler
 
 class SixChix(BaseComicCrawler):
 	def __init__(self):
-		super(SixChix, self).__init__('SixChix', 'cserver.it-content.com',80, 'Norske')
+		super(SixChix, self).__init__('Six Chix', 'cserver.it-content.com',80, 'Norske')
 		self.url = "http://cserver.it-content.com/retriever.php?id=31&date=%s" % (self.date.strftime("%Y%m%d"))
