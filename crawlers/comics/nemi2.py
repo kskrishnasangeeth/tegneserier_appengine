bbb296e886bec8c121697c1351e81e63d9372f15
Georg Alexander Boe <georg.boe@gmail.com>
2010-08-17 18:00:24 +0200

    Fix broken Nemi comic.

--- crawlers/comics/nemi2.py
+++ crawlers/comics/nemi2.py
@@ -5,4 +5,4 @@ class Nemi2(BaseComicCrawler):
 	def __init__(self):
 		super(Nemi2, self).__init__('Nemi', 'dagbladet.no', 50, 'Norske')
 		self.timezone = -2
-		self.url = "http://www.dagbladet.no/tegneserie/nemiarkiv/serve.php?%d" % (time.mktime(self.date.timetuple()))
+		self.url = "http://www.dagbladet.no/tegneserie/nemiarkiv/serve.php?%d" % (time.mktime(self.date.date().timetuple()))
