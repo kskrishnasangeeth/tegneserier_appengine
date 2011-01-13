bbf5facbf7e3f8e42a5270db8cc35aeab105232d
Georg Alexander Bøe <georg.boe@gmail.com>
2011-01-13 16:48:32 +0100

    Remove Rutetid.

--- crawlers/comics/rutetid.py
+++
@@ -1,6 +0,0 @@
-from crawlers.base import BaseComicCrawler
-
-class Rutetid(BaseComicCrawler):
-	def __init__(self):
-		super(Rutetid, self).__init__('Rutetid', 'nutria.msn.no', 70, 'Norske')
-		self.url = "http://nutria.msn.no/external/strand/rute_%s.jpg" % (self.date.strftime("%Y%m%d"))
