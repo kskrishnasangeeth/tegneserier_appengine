f6321abc6b23c2da0ba41a0fdb58c9792e9c990d
Georg Alexander Bøe <georg.boe@gmail.com>
2011-01-13 16:30:16 +0100

    Fix Tom Toles.

--- crawlers/comics/tomtoles.py
+++ crawlers/comics/tomtoles.py
@@ -1,6 +1,17 @@
 from crawlers.base import BaseComicCrawler
+from google.appengine.api import urlfetch
+from BeautifulSoup import BeautifulSoup
 
 class TomToles(BaseComicCrawler):
 	def __init__(self):
 		super(TomToles, self).__init__('Tom Toles', 'washingtonpost.com', 120, 'Utenlandske')
-		self.url = "http://www.washingtonpost.com/wp-srv/opinion/ssi/images/Toles/c_%s_520.gif" % (self.date.strftime("%m%d%Y"))
+		self.headers = { "Referer": 'http://www.gocomics.com/tomtoles/' }
+		self.url = self._imagelink()
+	
+	def _imagelink(self):
+		content = urlfetch.fetch('http://www.gocomics.com/tomtoles/').content
+		soup = BeautifulSoup(content)
+		for img in soup.findAll('img'):
+			if img["src"].startswith("http://imgsrv.gocomics.com/dim/?fh="):
+				return img["src"]
+		raise Exception
